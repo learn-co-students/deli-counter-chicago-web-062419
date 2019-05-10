9375ce505c23bc2ea78311f23fbebd0e6825e582
github username <github email address>
2019-05-10 13:06:50 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,32 @@
 def line(current_line)
     if current_line == []
         puts "The line is currently empty."
+    
     elsif current_line.length != 0
         the_line = []
-        the_line.push(current_line.each_with_index {|person, index| puts "#{index}. #{person}"})
-        puts the_line
-        puts "The line is currently #{the_line}."
+        current_line.each_with_index {|person, index| the_line.push("#{index + 1}. #{person}")}
+        puts "The line is currently: #{the_line.join(" ")}"
     end
 end
+
+def take_a_number(line, name)
+    if line == []
+        line.push(name)
+        puts "Welcome, #{name}. You are number #{line.length} in line."
+    elsif line != []
+        line.push(name)
+        puts "Welcome, #{name}. You are number #{line.length} in line."
+    end
+end
+
+def now_serving(line)
+    if line == []
+        puts "There is nobody waiting to be served!"
+
+    elsif line != []
+        puts "Currently serving #{line[0]}."
+        line.shift
+    end
+end
+
+
