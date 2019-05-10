0aac6d022abfc8ef3db4b5f8c0ef067a23427eb2
github username <github email address>
2019-05-10 11:41:00 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(current_line)
+    if current_line == []
+        puts "The line is currently empty."
+    elsif current_line.length != 0
+        the_line = []
+        the_line = the_line.push(current_line.each_with_index {|person, index| puts "#{index}. #{person}"})
+        the_line
+        puts "The line is currently #{the_line}."
+    end
+end
