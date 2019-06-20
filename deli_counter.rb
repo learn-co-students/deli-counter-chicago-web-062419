bda7aaa27a1ab44fd73fc79d7e5af8f9498a4443
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 21:05:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,7 @@ end
 
 def take_a_number(line_array, name)
   spot = line_array.size + 1
-  message = "Welcome, #{name}. You are number #{spot} in line."
-  puts message
+  puts "Welcome, #{name}. You are number #{spot} in line."
   line_array << name
 end
 
